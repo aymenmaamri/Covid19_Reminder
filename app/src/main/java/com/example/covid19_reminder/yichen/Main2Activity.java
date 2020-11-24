d416a0972c1ac2537e04a22d2a65385b58537818
Yichen Xie <843362901@qq.com>
2020-11-24 20:40:04 +0800

    Add mainActivity(Home Screen) and Map for Mask Sale Activity

---
+++ app/src/main/java/com/example/covid19_reminder/yichen/Main2Activity.java
@@ -0,0 +1,27 @@
+package com.example.covid19_reminder.yichen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.covid19_reminder.R;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    /** Called when the user taps the "Mask Sale" button */
+    public void maskSale(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+}
