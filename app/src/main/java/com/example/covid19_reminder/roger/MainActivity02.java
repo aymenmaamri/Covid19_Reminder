20fb321b5bbe4d68c94dc03c5e9e07523cc83477
rogerfokam <arnoldrodgers1@gmail.com>
2020-12-06 03:21:14 +0100

    Timer actualised

--- app/src/main/java/com/example/covid19_reminder/roger/MainActivity02.java
+++ app/src/main/java/com/example/covid19_reminder/roger/MainActivity02.java
@@ -1,7 +1,10 @@
 package com.example.covid19_reminder.roger;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
@@ -14,17 +17,20 @@ import android.widget.TextView;
 import com.example.covid19_reminder.R;
 
 import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity02 extends AppCompatActivity {
 
-        private static final long START_TIME_IN_MILLIS = 14400000;
-        private TextView mTextViewCountDown;
-        private Button mButtonStartPause;
-        private Button mButtonReset;
-        private CountDownTimer mCountDownTimer;
-        private boolean mTimerRunning;
-        private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    TextView timerText;
+    Button stopStartButton;
+
+    Timer timer;
+    TimerTask timerTask;
+    Double time = 0.0;
+
+    boolean timerStarted = false;
 
 
         @Override
@@ -32,70 +38,107 @@ public class MainActivity02 extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main02);
 
-            /** Called when the user taps the "START" and "PAUSE" button */
+            timerText = (TextView) findViewById(R.id.timerText);
+            stopStartButton = (Button) findViewById(R.id.startStopButton);
 
-            mTextViewCountDown = findViewById(R.id.text_view_countdown);
-            mButtonStartPause = findViewById(R.id.button_start_pause);
-            mButtonReset = findViewById(R.id.button_reset);
-            mButtonStartPause.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    /** verifed when the time are running*/
-                    if (mTimerRunning) {
-                        pauseTimer();
-                    } else {
-                        startTimer();
-                    }
-                }
-            });
-            /** Called when the user taps the "RESET"  button */
-            mButtonReset.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    resetTimer();
-                }
-            });
-            updateCountDownText();
+            timer = new Timer();
         }
-        private void startTimer() {
-            mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
-                @Override
-                public void onTick(long millisUntilFinished) {
-                    mTimeLeftInMillis = millisUntilFinished;
-                    updateCountDownText();
-                }
-                @Override
-                public void onFinish() {
-                    mTimerRunning = false;
-                    mButtonStartPause.setText("Start");
-                    mButtonStartPause.setVisibility(View.INVISIBLE);
-                    mButtonReset.setVisibility(View.VISIBLE);
-                }
-            }.start();
-            mTimerRunning = true;
-            mButtonStartPause.setText("pause");
-            mButtonReset.setVisibility(View.INVISIBLE);
-        }
-        private void pauseTimer() {
-            mCountDownTimer.cancel();
-            mTimerRunning = false;
-            mButtonStartPause.setText("Start");
-            mButtonReset.setVisibility(View.VISIBLE);
-        }
-        private void resetTimer() {
-            mTimeLeftInMillis = START_TIME_IN_MILLIS;
-            updateCountDownText();
-            mButtonReset.setVisibility(View.INVISIBLE);
-            mButtonStartPause.setVisibility(View.VISIBLE);
-        }
+    public void resetTapped(View view)
+    {
+        AlertDialog.Builder resetAlert = new AlertDialog.Builder(this);
+        resetAlert.setTitle("Reset Timer");
+        resetAlert.setMessage("Are you sure you want to reset the timer?");
+        resetAlert.setPositiveButton("Reset", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                if(timerTask != null)
+                {
+                    timerTask.cancel();
+                    setButtonUI("START", R.color.green);
+                    time = 0.0;
+                    timerStarted = false;
+                    timerText.setText(formatTime(0,0,0));
 
-    /** das muss noch gemacht verden: Es gibt hier Rechnungsprobleme */
-        private void updateCountDownText() {
-            int heures = (int) (mTimeLeftInMillis / 1000) / 3600;
-            int minutes = (int) (mTimeLeftInMillis / 1000) / 240;
-            int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
-            String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d:%02d", heures, minutes, seconds);
-            mTextViewCountDown.setText(timeLeftFormatted);
-        }
+                }
+            }
+        });
+
+        resetAlert.setNeutralButton("Cancel", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                //do nothing
+            }
+        });
+
+        resetAlert.show();
 
     }
+
+    public void startStopTapped(View view)
+    {
+        if(timerStarted == false)
+        {
+            timerStarted = true;
+            setButtonUI("STOP", R.color.red);
+
+            startTimer();
+        }
+        else
+        {
+            timerStarted = false;
+            setButtonUI("START", R.color.green);
+
+            timerTask.cancel();
+        }
+    }
+
+    private void setButtonUI(String start, int color)
+    {
+        stopStartButton.setText(start);
+        stopStartButton.setTextColor(ContextCompat.getColor(this, color));
+    }
+
+    private void startTimer()
+    {
+        timerTask = new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        time++;
+                        timerText.setText(getTimerText());
+                    }
+                });
+            }
+
+        };
+        timer.scheduleAtFixedRate(timerTask, 0 ,1000);
+    }
+
+
+    private String getTimerText()
+    {
+        int rounded = (int) Math.round(time);
+
+        int seconds = ((rounded % 86400) % 3600) % 60;
+        int minutes = ((rounded % 86400) % 3600) / 60;
+        int hours = ((rounded % 86400) / 3600);
+
+        return formatTime(seconds, minutes, hours);
+    }
+
+    private String formatTime(int seconds, int minutes, int hours)
+    {
+        return String.format("%02d",hours) + " : " + String.format("%02d",minutes) + " : " + String.format("%02d",seconds);
+    }
+
+}
