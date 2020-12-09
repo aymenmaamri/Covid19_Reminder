7a2cecd7b3c6ddc1d71a857e851dee6b2c350220
yichenx1 <yichenxie627@gmail.com>
2020-12-09 23:02:21 +0800

    Maps: Ask for permissions and let the app know the user's current location

--- app/src/main/java/com/example/covid19_reminder/yichen/Main2Activity.java
+++ app/src/main/java/com/example/covid19_reminder/yichen/Main2Activity.java
@@ -11,36 +11,15 @@ import com.example.covid19_reminder.R;
 import com.example.covid19_reminder.aymen.SettingsActivity;
 
 import com.example.covid19_reminder.roger.MainActivity01;
-import com.example.covid19_reminder.roger.MainActivity02;
 
 public class Main2Activity extends AppCompatActivity {
 
     public Button timer;
-    Button btn1;
-    Button btn2;
-    Button btn3;
-    Button btn4;
-    Button btn5;
-
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
-        /** Called when the user taps the "TIMER" button */
-
-        /**this.timer = (Button) findViewById(R.id.timer);
-        timer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent otherActivity = new Intent(getApplicationContext(), MainActivity01.class);
-                startActivity(otherActivity);
-                finish();
-            }
-        });**/
-
-
 
     }
 
@@ -56,97 +35,11 @@ public class Main2Activity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    /** Called when the user taps the "Settings" button */
-    public void settings(View view)
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.notifications1);
-
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openmaps();
-            }
-        });
+    /**
+     * Called when the user taps the "Settings" button
+     */
+    public void settings(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
-            private void openmaps() {
-                Intent intent = new Intent(this, MapsActivity.class);
-                startActivity (intent);
-            }
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.notifications1);
-
-        btn2 = (Button) findViewById(R.id.btn2);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openhomepage();
-            }
-        });
-    }
-    private void openhomepage() {
-        Intent intent = new Intent(this, activity_main2.xml);
-        startActivity (intent);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.notifications2);
-
-        btn3 = (Button) findViewById(R.id.btn3);
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                opentimer();
-            }
-        });
-    }
-    private void opentimer() {
-        Intent intent = new Intent(this, MainActivity02.class);
-        startActivity (intent);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.notification3);
-
-        btn4 = (Button) findViewById(R.id.btn4);
-        btn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                opentimer();
-            }
-        });
-    }
-    private void opentimer() {
-        Intent intent = new Intent(this, MainActivity02.class);
-        startActivity (intent);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.notification3);
-
-        btn5 = (Button) findViewById(R.id.btn5);
-        btn5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openmaps();
-            }
-        });
-    }
-    private void openmaps() {
-        Intent intent = new Intent(this, Main2Activity.class)
-        startActivity (intent);
-    }
-    }
+}
